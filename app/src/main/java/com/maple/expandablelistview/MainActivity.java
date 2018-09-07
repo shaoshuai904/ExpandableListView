434cdfd75099d4a03b2d7c5ce4fc8887da6707ce
MapleShao <939078792@qq.com>
2018-09-07 17:42:04 +0800

    add ExpandableLayoutListView.

--- app/src/main/java/com/maple/expandablelistview/MainActivity.java
+++ app/src/main/java/com/maple/expandablelistview/MainActivity.java
@@ -2,12 +2,30 @@ package com.maple.expandablelistview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 
+import com.maple.expandablelistview.library.ExpandableLayoutListView;
+
+/**
+ * @author maple
+ * @time 2018/9/7.
+ */
 public class MainActivity extends AppCompatActivity {
+
+
+    private String[] array = {"Hello", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome"};
+
+    ExpandableLayoutListView expandableLayoutListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        expandableLayoutListView = (ExpandableLayoutListView) findViewById(R.id.listview);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.view_row, R.id.header_text, array);
+
+        expandableLayoutListView.setAdapter(arrayAdapter);
     }
+
 }
